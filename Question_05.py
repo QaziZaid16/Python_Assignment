49aeda0b39cb4c310dad6f2ea2a2f8c4385fd7f1
QaziZaid16 <146083607+QaziZaid16@users.noreply.github.com>
2024-09-11 23:37:19 +0530

    Create Question_05.py

---
+++ Question_05.py
@@ -0,0 +1,8 @@
+if isAnagram(str1, str2):  
+    print("The two strings are anagrams.")  
+else:  
+    print("The two strings are not anagrams.")  
+
+str1 = "Lists"
+str2 = "Relent"
+Check (str1, str2)
